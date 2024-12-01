76472acb3e7cdf21fc091192a7b8a17fe9ddfc7a
Fatima Salman <fati.salman31@gmail.com>
2024-12-01 23:09:35 +0500

    first commit

---
+++ app/about/page.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>
+        <h1 id='heading1'>
+        Welcome To About Page
+        </h1>
+        <div id='about'>
+            <div  id='a1'>A</div>
+            <div id='b'>B</div>
+            <div id='o1'>O</div>
+            <div id='u'>U</div>
+            <div id='t1'>T</div>
+           
+        </div>
+    </div>
+    
+  )
+}
+
+export default About
+
+
---
+++ app/contact/page.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <div >
+      <h1 id='h2'>Welcome To contact Page</h1>
+
+<div id="contact">
+  <div id='c'>C</div>
+  <div id='o2'>O</div>
+  <div id='n'>N</div>
+  <div id='t2'>T</div>
+  <div id='a2'>A</div>
+  <div id='c2'>C</div>
+  <div id='t3'>T</div>
+</div>
+
+    </div>
+  )
+}
+
+export default Contact
